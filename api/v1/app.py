b50717e4b3b968783f0c7bf32925a0740498c1a4
Florencecoker <damselpiccolo18@gmail.com>
2023-05-02 20:53:41 -0700

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-'''
-    app for registering blueprint and starting flask
-'''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from models import storage
+"""ALX SE Flask Api Module."""
 from api.v1.views import app_views
-from os import getenv
-
+from flask import Flask, jsonify, make_response
+from models import storage
+import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def tear_down(self):
-    '''
-    close query after each session
-    '''
-    storage.close()
-
-
 @app.errorhandler(404)
-def not_found(error):
-    '''
-    return JSON formatted 404 status code response
-    '''
+def not_found(exception=None):
+    """Handled resource not found error."""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+@app.teardown_appcontext
+def close_session(exception=None):
+    """Close current session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=HOST, port=PORT, threaded=True)
